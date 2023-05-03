197d588d9072a2ede91a481227ba19b94eb22ac0
zpolonius <zacharias@polonius.dk>
2023-05-03 13:29:15 +0200

    removed # Set the current date and format it for the URL current_date = datetime.datetime.today().strftime('%Y-%m-%d')

--- download.py
+++ download.py
@@ -6,15 +6,6 @@ import os
 # Set the base URL and initial ID value
 base_url = ".altapaysecure.com/merchant.php/API/fundingDownload?id="
 
-# Set the current date and format it for the URL
-current_date = datetime.datetime.today().strftime('%Y-%m-%d')
-
-# Set the filename to use for the downloaded file
-folder_name = "funding filer"
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-
-filename = os.path.join(folder_name, f"funding_{current_date}.csv")
 #User inputs
 # Set your Basic Authentication credentials
 username = input('Enter username: ')
@@ -23,9 +14,16 @@ password = input('Enter the password: ')
 # Ask the user for the ID value
 id_value = int(input("Enter the ID value: "))
 
+
+# Set the filename to use for the downloaded file
+folder_name = "funding filer"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+filename = os.path.join(folder_name, f"funding_{id_value}.csv")
+
 #Set the domain
 domain_url = 'altapay'
-
 
 # Encode the credentials as base64
 auth = base64.encodebytes(f"{username}:{password}".encode()).decode().strip()
